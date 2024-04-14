953b1de13d69620bd92f8a99e541c4f76738ed85
Galymzhan <90953919+Kalizhanov@users.noreply.github.com>
2024-04-14 22:31:41 +0500

    [dp-3] feat: add views of forgot password (#11)

--- backend/authorization/urls.py
+++ backend/authorization/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
 
     path('enter-email/', views.enter_email, name='enter-email'),
     path('verify-email/', views.verify_email, name='verify-email'),
+    path('reset/', views.forgot_password, name='forgot_password'),
+    path("verify-by-code", views.verify_by_code, name="verify-by-code"),
+    path('set-password/', views.set_new_password, name='set_new_password'),
 ]
