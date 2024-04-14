3ed322aeb1a2c28597273207748dfe0c58d17c26
Kalizhanov Galymzhan <galymzhanks@gmail.com>
2024-04-14 22:41:36 +0500

    [dp-34] add face control to employee login

--- backend/authorization/views.py
+++ backend/authorization/views.py
@@ -1,13 +1,17 @@
 import json
+import cv2
+import face_recognition
+import numpy as np
 
 from django.contrib import messages, auth
 from django.contrib.auth.models import AnonymousUser
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpRequest
 from django.shortcuts import render, redirect
 from django.http.request import HttpRequest
 
-from .models import User
-from .utils import send_email, verify_account, send_link
+from authorization import UserRoles
+from authorization.models import User
+from authorization.utils import send_email, verify_account, send_link
 
 
 def login(request: HttpRequest):
@@ -16,6 +20,7 @@ def login(request: HttpRequest):
         password = request.POST.get('password')
 
         user: User = User.objects.filter(mobile_phone=mobile_phone).first()
+        print(mobile_phone)
 
         if user is None:
             messages.error(request, 'User with this phone number does not exist!')
@@ -24,6 +29,11 @@ def login(request: HttpRequest):
         if not user.check_password(password):
             messages.error(request, 'Password is incorrect!')
             return redirect('login')
+
+        if user.role == UserRoles.EMPLOYEE:
+            face_control(user)
+            # if face_control(user) != user.full_name:
+            #     return JsonResponse({"error": "THERE IS ERROR"})
 
         auth.login(request, user)
         user.save_login_days()
@@ -165,3 +175,68 @@ def set_new_password(request: HttpRequest):
         return redirect("login")
     
     return JsonResponse({"error": "Not Allowed Method", "status": 405})
+
+
+def face_control(user: User):
+
+    # video_capture = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+    video_capture = cv2.VideoCapture(0)
+
+    image = face_recognition.load_image_file(user.user_info.photo_avatar)
+    image_encoding = face_recognition.face_encodings(image)[0]
+
+    know_face_encodings = [image_encoding]
+    know_face_names = [user.full_name]
+
+    face_locations = []
+    face_encodings = []
+    face_names = []
+    process_this_frame = True
+
+    while True:
+        ret, frame = video_capture.read()
+
+        if process_this_frame:
+            small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+            rgb_small_frame = small_frame[:, :, ::-1]
+
+            face_locations = face_recognition.face_locations(rgb_small_frame)
+            face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+
+            face_names = []
+            for face_encoding in face_encodings:
+
+                matches = face_recognition.compare_faces(know_face_encodings, face_encoding)
+                name = "Unkown"
+
+                face_distances = face_recognition.face_distance(know_face_encodings, face_encoding)
+                best_match_index = np.argmin(face_distances)
+                if matches[best_match_index]:
+                    name = know_face_names[best_match_index]
+
+                face_names.append(name)
+        
+        process_this_frame = not process_this_frame
+        # return face_names[0]
+    
+        for (top, right, bottom, left), name in zip(face_locations, face_names):
+            top *= 4
+            right *= 4
+            bottom *= 4
+            left *= 4
+
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+        cv2.imshow('Video', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    video_capture.release()
+    cv2.destroyAllWindows()
+
+    return face_names[0]
